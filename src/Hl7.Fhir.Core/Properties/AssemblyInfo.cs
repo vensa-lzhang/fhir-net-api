94ff08d21661428d09207cc1e748d8d290543aef
Alex Zhang <lzhang@vensahealth.com>
2015-12-09 12:13:28 +1300

    Update fhir .net library

--- src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
+++ src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
@@ -18,13 +18,19 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 #if DEBUG
-[assembly: InternalsVisibleTo("Hl7.Fhir.Core.Tests")]
+//Assembly "Hl7.Fhir.Core" has been strong-name signed to allow safe distribution to third-party developers
+//creating their own plugins.
+//I am commenting the next line because strong-name signed assemblies must specify a public key in their 
+//InternalsVisibleTo declarations to reference friend assemblies that have not been strong-name signed.
+//If you uncomment the next line make sure to either include the public key or strong-name sign 
+//friend assembly "Hl7.Fhir.Core.Tests".
+//[assembly: InternalsVisibleTo("Hl7.Fhir.Core.Tests")]
 #endif
 
 #if RELEASE
-[assembly:AssemblyKeyFileAttribute("..\\FhirNetApi.snk")]
+//[assembly:AssemblyKeyFileAttribute("..\\FhirNetApi.snk")]
 #endif
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.90.3.*")]
+[assembly: AssemblyVersion("0.90.3.0")]
